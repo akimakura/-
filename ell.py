b03e97810ab4e09d2eec00658bd40639bcd685d6
akimakura <akimik2002@gmail.com>
2024-09-03 16:07:08 +0300

    Немного почистил код

--- ell.py
+++ ell.py
@@ -4,19 +4,11 @@ import eel
 eel.init("web")
 
 @eel.expose
-def minus(one_number, two_number):
+def minus(one_number: str, two_number: str) -> str:
     try:
-        # Для начала используем числа, чтобы проверить вводимые данные
-        one_number = int(one_number)
-        two_number = int(two_number)
-
-        # Ограничение чисел(НЕ отрицательное, НЕ более 256 символов)
-        if one_number < 0 or two_number < 0 or len(str(one_number)) > 256 or len(str(two_number)) > 256:
-            raise ValueError("Число отрицательное или превышает 256 символов.")
-
-        # Преобразуем числа в строки
-        one_number = str(one_number)
-        two_number = str(two_number)
+        #  Проверка размера числа
+        if len(one_number) >= 256 or len(two_number) >= 256:
+            raise ValueError("Число превышает 256 символов.")
 
         # Находим большее число для правильности рассчета
         max_len = max(len(one_number), len(two_number))
